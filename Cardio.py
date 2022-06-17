209f3cbe2a0e6dbf717fb14e6dae75dcb95198d9
Amreh43522 <107275776+Amreh43522@users.noreply.github.com>
2022-06-17 16:32:18 +0200

    Update Cardio.py

--- Cardio.py
+++ Cardio.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 
 class Cardio(BaseModel):
-    gender1-fand2-m: float 
-    heightincm: float 
-    weightincm: float 
+    gender: float 
+    height: float 
+    weight: float 
     systolic: float
     diastolic: float
     age: float
     cholesterol: int
     glucose_lvl: int
-    smoke0-non1-smoker: int
+    smoke: int
     active: int
